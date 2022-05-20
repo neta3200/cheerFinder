50477c1db283ef2d9bd46079a4e1917ff9542bc7
neta3200 <neta3200@gmail.com>
2022-05-20 14:06:36 +0300

    first commit

--- css/data/stores.js
+++ css/data/stores.js
@@ -1,11 +1,14 @@
 var jsonStores= {
-    "type": "FeatureCollection",
-    "name": "Hotels_1",
-    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-    "features": [
+"type": "FeatureCollection",
+"name": "Hotels_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
 
 
+{ "type": "Feature", "properties": { "Name": "מבחר המשקאות", "Address": "שלמה אבן גבירול 97,,תל אביב יפו", "openingSunday":"09:00-21:00","openingMonday":"09:00-21:00","openingTuesday":"09:00-21:00","openingWednesday":"09:00-21:00","openingThursday":"09:00-21:00","openingFriday":"09:00-21:00",,"openingSaturday":"סגור","telephone":"03-5249341","link": "https:\/\/drinks4u.co.il", "stars": 5.0,"orderShipping":"כן","orderCollection":"כן","orderOnSpot":"כן" , "Image":"images\stores\mivhar_amashkaot.jpg" }, "geometry": { "type": "Point", "coordinates": [ 32.0858125, 34.7816875 ] } },
+{ "type": "Feature", "properties": { "Name": "אפריטיף חנויות יין", "Address": "דיזנגוף 93, תל אביב-יפו", "openingSunday":"09:00-21:00","openingMonday":"09:00-21:00","openingTuesday":"09:00-21:00","openingWednesday":"09:00-21:00","openingThursday":"09:00-21:00","openingFriday":"09:00-21:00",,"openingSaturday":"סגור","telephone":"03-5249341","link": "https:\/\/drinks4u.co.il", "stars": 5.0,"orderShipping":"כן","orderCollection":"כן","orderOnSpot":"כן" , "Image":"images\stores\mivhar_amashkaot.jpg" }, "geometry": { "type": "Point", "coordinates": [ 32.05125, 34.5816875 ] } },
+{ "type": "Feature", "properties": { "Name": "מבחר המשקאות", "Address": "שלמה אבן גבירול 97,,תל אביב יפו", "openingSunday":"09:00-21:00","openingMonday":"09:00-21:00","openingTuesday":"09:00-21:00","openingWednesday":"09:00-21:00","openingThursday":"09:00-21:00","openingFriday":"09:00-21:00",,"openingSaturday":"סגור","telephone":"03-5249341","link": "https:\/\/drinks4u.co.il", "stars": 5.0,"orderShipping":"V","orderCollection":"V","orderOnSpot":"V" , "Image":"images\stores\mivhar_amashkaot.jpg" }, "geometry": { "type": "Point", "coordinates": [ 32.0858125, 34.7816875 ] } },
+{ "type": "Feature", "properties": { "Name": "מבחר המשקאות", "Address": "שלמה אבן גבירול 97,,תל אביב יפו", "openingSunday":"09:00-21:00","openingMonday":"09:00-21:00","openingTuesday":"09:00-21:00","openingWednesday":"09:00-21:00","openingThursday":"09:00-21:00","openingFriday":"09:00-21:00",,"openingSaturday":"סגור","telephone":"03-5249341","link": "https:\/\/drinks4u.co.il", "stars": 5.0,"orderShipping":"V","orderCollection":"V","orderOnSpot":"V" , "Image":"images\stores\mivhar_amashkaot.jpg" }, "geometry": { "type": "Point", "coordinates": [ 32.0858125, 34.7816875 ] } },
 { "type": "Feature", "properties": { "Name": "מבחר המשקאות", "Address": "שלמה אבן גבירול 97,,תל אביב יפו", "openingSunday":"09:00-21:00","openingMonday":"09:00-21:00","openingTuesday":"09:00-21:00","openingWednesday":"09:00-21:00","openingThursday":"09:00-21:00","openingFriday":"09:00-21:00",,"openingSaturday":"סגור","telephone":"03-5249341","link": "https:\/\/drinks4u.co.il", "stars": 5.0,"orderShipping":"V","orderCollection":"V","orderOnSpot":"V" , "Image":"images\stores\mivhar_amashkaot.jpg" }, "geometry": { "type": "Point", "coordinates": [ 32.0858125, 34.7816875 ] } }
-
 ]
 }
